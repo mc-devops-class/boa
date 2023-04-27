0ce07dff822070a9ff1baac7955a911562bdd66c
tonidavis1738 <tonidavis1738@gmail.com>
2023-04-27 06:32:15 +0000

    new update added

---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This script is used to execute numerous commands at once
+echo "The lord is good"
+echo "I will be wealthy before the end of"
+date +%Y
+echo "Beacause God's favor is upon my life"
